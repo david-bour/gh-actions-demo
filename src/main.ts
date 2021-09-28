64b8ed962c9be968c0b3590da30af8f00142c296
David <dbour.developer@gmail.com>
2021-09-27 21:47:41 -0700

    read stdout

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ const github = require('@actions/github');
 
 async function run() {
     const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toDateString();
+    core.setOutput("time", time);
     const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
 }
 
 run().catch(error => core.setFailed('Workflow failed ' + error.message));
